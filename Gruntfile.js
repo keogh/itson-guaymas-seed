a167fbdfb6dae57ecbd1380cf706725452aef1dc
Isaac Zepeda <keogh24@gmail.com>
2014-03-20 17:30:23 -0700

    Configure connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,18 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          hostname: 'localhost',
+          livereload: 35729,
+          base: 'app',
+          keepalive: true
+        }
+      }
+    }
   });
+
+  grunt.registerTask('server', ['connect']);
 }
