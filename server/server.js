2ea37bf83682d3576e97c00ffeabd3c938b1c4a1
Isaac Zepeda <keogh24@gmail.com>
2014-03-21 10:14:41 -0700

    Create server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var io = require("socket.io").listen(8080);
+var users = [];
+
+io.sockets.on('connection', function (socket) {
+  socket.on('username', function (username) {
+    users.push(username);
+    io.sockets.emit('users', users);
+
+    socket.on('msg', function (msg) {
+      io.sockets.emit('msg', username, msg);
+    });
+
+    socket.on('disconnect', function () {
+      users.splice(users.indexOf(username), 1);
+      io.sockets.emit('users', users);
+    });
+  });
+});
