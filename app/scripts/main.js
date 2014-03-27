167a2fa9babc84681d973fa8dd011ff777f4e8a7
Isaac Zepeda <keogh24@gmail.com>
2014-03-26 17:43:47 -0700

    Can create a message

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,14 +9,19 @@ angular.module('chat-app').controller('MainCtrl', function ($scope) {
     {username: 'Jesus'}
   ];
 
-  $scope.messages = [
-    {
-      username: 'Isaac',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
-    },
-    {
-      username: 'Alberto',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
-    }
-  ];
+  $scope.messages = [];
+  $scope.currentUser = {
+    username: 'Isaac',
+    message: ''
+  }
+
+  $scope.send = function () {
+    if ($scope.currentUser.message === '') return;
+    // TODO: Send message to server
+    $scope.messages.push({
+      username: $scope.currentUser.username,
+      content: $scope.currentUser.message
+    });
+    $scope.currentUser.message = '';
+  }
 });
