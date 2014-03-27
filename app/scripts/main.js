840d438595e493533a55b7e312fa8ea7d13abad9
Isaac Zepeda <keogh24@gmail.com>
2014-03-26 17:37:20 -0700

    Angularize users list

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,11 @@
 angular.module('chat-app').controller('MainCtrl', function ($scope) {
-  
+  $scope.users = [
+    {username: 'Isaac'},
+    {username: 'Alberto'},
+    {username: 'Miguel'},
+    {username: 'Roberto'},
+    {username: 'Jorge'},
+    {username: 'Manuel'},
+    {username: 'Jesus'}
+  ]
 });
